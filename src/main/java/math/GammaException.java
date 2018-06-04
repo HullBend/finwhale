ca065e46bcb5b087e754f3bf2dceb6a2dffec09c
HullBend <HullBend@users.noreply.github.com>
2018-06-05 01:51:21 +0200

    strip down constructors

--- src/main/java/math/GammaException.java
+++ src/main/java/math/GammaException.java
@@ -22,14 +22,6 @@ public final class GammaException extends ArithmeticException {
 
     private static final long serialVersionUID = 6738428636131168196L;
 
-    public GammaException(final String msg) {
-        super(msg);
-    }
-
-    public GammaException(final String msg, final int location) {
-        super(msg + " : loc:" + location);
-    }
-
     public GammaException(final String msg, final String reason, final int location) {
         super(msg + " : reason = \"" + reason + "\", loc:" + location);
     }
