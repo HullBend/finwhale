38c8a840b5f23e8326e965d6577e678c7cdd15ef
HullBend <HullBend@users.noreply.github.com>
2018-06-05 21:22:13 +0200

    goodness-of-fit

--- src/main/java/math/stats/distribution/fit/UniformTestStatistics.java
+++ src/main/java/math/stats/distribution/fit/UniformTestStatistics.java
@@ -67,6 +67,63 @@ public final class UniformTestStatistics {
          * Number of observations
          */
         public int N = -1;
+
+        public String toString() {
+            StringBuilder b = new StringBuilder(256);
+            b.append("\r\n");
+            // b.append("KSP D+ : ").append(KSP).append("\r\n");
+            // b.append("KSM D- : ").append(KSM).append("\r\n");
+            b.append("KS  D  : ").append(KS).append("\r\n");
+            b.append("AD  A2 : ").append(AD).append("\r\n");
+            // b.append("CM W2 : ").append(CM).append("\r\n");
+            // b.append("WG G : ").append(WG).append("\r\n");
+            // b.append("WU U2 : ").append(WU).append("\r\n");
+            b.append("MEAN   : ").append(MEAN).append("\r\n");
+            b.append("N      : ").append(N).append("\r\n\r\n");
+            return b.toString();
+        }
+    }
+
+    /**
+     * TODO
+     */
+    public static final class PValue {
+        /**
+         * Kolmogorov-Smirnov+ test p-value
+         */
+        public double KSP_PVAL = Double.NaN;
+        /**
+         * Kolmogorov-Smirnov- test p-value
+         */
+        public double KSM_PVAL = Double.NaN;
+        /**
+         * Kolmogorov-Smirnov test p-value
+         */
+        public double KS_PVAL = Double.NaN;
+        /**
+         * Anderson-Darling test p-value
+         */
+        public double AD_PVAL = Double.NaN;
+        /**
+         * Number of observations
+         */
+        public int N = -1;
+
+        public String toString() {
+            String ksp = "KolmogorovSmirnovPlus  p-value: " + KSP_PVAL;
+            String ksm = "KolmogorovSmirnovMinus p-value: " + KSM_PVAL;
+            String ks = "KolmogorovSmirnov      p-value: " + KS_PVAL;
+            String ad = "Anderson-Darling       p-value: " + AD_PVAL;
+            String size = "Sample size                 : " + N;
+            StringBuilder b = new StringBuilder(384);
+            b.append("\r\n");
+            // b.append(ksp).append("\r\n");
+            // b.append(ksm).append("\r\n");
+            b.append(ks).append("\r\n");
+            b.append(ad).append("\r\n");
+            b.append(size).append("\r\n\r\n");
+            return b.toString();
+        }
     }
 
     private static final double EPS = MathConsts.BIG_INV / 2.0;
@@ -89,6 +146,7 @@ public final class UniformTestStatistics {
         if (obs.length == 1) {
             statistic.KSP = 1.0 - obs[0];
             statistic.MEAN = obs[0];
+            statistic.N = 1;
             return statistic;
         }
 
