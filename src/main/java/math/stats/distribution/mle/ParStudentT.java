06599e2bfa498f346dbd03f1f63ef9ed883f3fcf
HullBend <HullBend@users.noreply.github.com>
2018-06-10 20:17:53 +0200

    StudentT MLE parameter

---
+++ src/main/java/math/stats/distribution/mle/ParStudentT.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018 SPZ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package math.stats.distribution.mle;
+
+import math.Arithmetic;
+import math.stats.ValidatedValue;
+import math.stats.distribution.StudentT;
+
+/**
+ * MLE for the parameter of the {@link StudentT} distribution.
+ */
+public final class ParStudentT implements ValidatedValue {
+
+    /** degrees of freedom (&nu;) */
+    public double df = Double.NaN;
+
+    @Override
+    public boolean isValid() {
+        return !Arithmetic.isBadNum(df);
+    }
+}
